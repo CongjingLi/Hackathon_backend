a1c55c2a35649c647bc5a4b73b7636bcc31e14da
ljm <646505418@qq.com>
2023-09-22 18:33:12 +0800

    hackathon: complete backend logics

--- src/main/java/com/citi/hackathon_backend/userInfo/UserRepository.java
+++ src/main/java/com/citi/hackathon_backend/userInfo/UserRepository.java
@@ -1,10 +1,11 @@
 package com.citi.hackathon_backend.userInfo;
 
 
+import com.citi.hackathon_backend.userInfo.entity.UserInfo;
 import org.springframework.data.repository.CrudRepository;
 
 @org.socialsignin.spring.data.dynamodb.repository.EnableScan
-public interface UserRepository extends CrudRepository {
+public interface UserRepository extends CrudRepository<UserInfo,String> {
 
 
 }
